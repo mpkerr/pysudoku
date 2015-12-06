26556263801c75ada54e4913060b72c260be873f
Martin Kerr <martin@pixability.com>
2015-12-06 13:17:03 -0500

    create boards and games with given difficulty

--- games.py
+++ games.py
@@ -1,4 +1,4 @@
-from sudoku import Board
+from sudoku import Board, IllegalBoard, V, N
 from game import Game, Move
 from random import choice, shuffle
 from itertools import product
@@ -13,15 +13,19 @@ def random_board():
         return next(map(lambda x: list(zip([c.coord for c in row], x)),
                         filter(lambda x: len(x) == len(set(x)), product(*values))))
 
-    b = Board()
-    for cell in b.rows[0]:
-        cell.value = choice(list(cell.values))
+    while True:
+        try:
+            b = Board()
+            for cell in b.rows[0]:
+                cell.value = choice(list(cell.values))
 
-    for row in b.rows[1:]:
-        for c, v in randrow(row):
-            b(*c).value = v
+            for row in b.rows[1:]:
+                for c, v in randrow(row):
+                    b(*c).value = v
 
-    return b
+            return b
+        except IllegalBoard:
+            pass
 
 
 def board(marking):
@@ -31,6 +35,37 @@ def board(marking):
     return b
 
 
+def create_board(difficulty, b=None):
+    if b is None:
+        b = random_board()
+
+    def rank(b):
+        from math import ceil, log
+        return ceil(log(b.complexity()))
+
+    while rank(b) < difficulty:
+        # pick a value to erase
+        value = choice(list(V))
+
+        # pick a number of cells from which to erase this value
+        count = choice(range(N))
+
+        # pick random cells to erase
+        cells = list(filter(lambda x: x.value == value, b))
+        shuffle(cells)
+        for cell in cells[:count]:
+            cell.value = None
+
+    return b
+
+
 def game(marking):
     return Game(Move(marking))
 
+
+def game_from_board(b):
+    return game(b.marking)
+
+
+def random_game(difficulty):
+    return game_from_board(create_board(difficulty))
