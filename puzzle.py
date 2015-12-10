2aec30ae4eac3a3dfeaa724d931d0fc2c565dd62
Martin Kerr <martin@pixability.com>
2015-12-09 19:32:34 -0500

    formatting

--- puzzle.py
+++ puzzle.py
@@ -1,12 +1,12 @@
 def load_su(filename):
     marking = []
-    with open(filename,'r') as file:
+    with open(filename, 'r') as file:
         row = 0
         for line in filter(None,map(str.strip,file)):
             column = 0
             for token in line.split():
                 if token not in "_0.":
-                    marking.append(((row,column), int(token)))
+                    marking.append(((row, column), int(token)))
                 column += 1
 
             row += 1
